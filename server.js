02c3f7fb43195322093c3a387ba63821c8ffe385
Jon Brennan <jonbrennan@Jons-MacBook-Pro.local>
2023-09-13 19:11:09 -0400

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8008;
+const peopleController = require("./controllers/peopleController");
+const morgan = require("morgan");
+app.use(morgan("tiny"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.use("/people", peopleController);
+
+app.listen(PORT, () => console.log("PORT numero", PORT));
